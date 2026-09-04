baa4276e7879cf6667d3cd181fbc65b1fc7d9d7c
MohammedThaher01 <113426580+MohammedThaher01@users.noreply.github.com>
2026-09-04 12:27:52 +0530

    Update import path for HuggingFaceEmbeddings

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
 from langchain.chains import ConversationalRetrievalChain
